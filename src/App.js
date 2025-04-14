d086c12f1716693d41da2dffe0338d88e3e330c3
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-04-14 16:43:11 +0200

    create function App - add component (content for pages) - add some css styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://img.freepik.com/premium-vector/hanami-japanese-flower-logo-design-cherry-blossom-vector-illustration_691652-143.jpg"
+          alt=""
+        ></img>
       </header>
+      <nav className="nav">
+        <div>
+          <a href="/"> Profile</a>
+        </div>
+        <div>
+          <a href="/">Messages</a>
+        </div>
+        <div>
+          <a href="/">News</a>
+        </div>
+        <div>
+          <a href="/">Music</a>
+        </div>
+        <div>
+          <a href="/">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <img
+          src="https://habrastorage.org/getpro/habr/upload_files/8e9/63c/507/8e963c50758de1ed63c11e1e8dcfe916.jpg"
+          alt="Япония"
+        ></img>
+      </div>
     </div>
   );
 }
