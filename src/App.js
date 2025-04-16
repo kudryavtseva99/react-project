610df0512b2b318042bcc1e9e27299bd5c81d17b
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-04-16 23:43:05 +0200

    add state active for dialoges elements

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/dialogs/*" element={<Dialogs />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,97 @@
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div className={s.item}>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div>
+          <NavLink
+            to="/dialogs/1"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Dima
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/2"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Sasha
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/3"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Artur
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/4"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Julia
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/5"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Roma
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/6"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Elena
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/7"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Nastya
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/8"
+            className={({ isActive }) =>
+              isActive ? `${s.dialog} ${s.activeItem}` : s.dialog
+            }
+          >
+            Valeria
+          </NavLink>
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi! How are you?</div>
+        <div className={s.message}>What's your hobby?</div>
+        <div className={s.message}>How is your day today?</div>
+      </div>
+    </div>
+  );
 };
-
 export default Dialogs;
