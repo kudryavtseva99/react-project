7b1abd7c8ad22c153131dd67272e54a601e91ceb
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-06-18 13:31:44 +0200

    add text area and btn in dialogues

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import s from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Messages/Message";
+import React from "react";
 
 const Dialogs = (props) => {
   let dialogsElements = props.state.dialogsData.map((dialog) => (
@@ -16,10 +17,24 @@ const Dialogs = (props) => {
     <Message message={m.message} key={m.id} />
   ));
 
+  let newMessage = React.createRef();
+
+  let addNewMessage = () => {
+    let text = newMessage.current.value;
+    alert(text);
+  };
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
       <div className={s.messages}>{messagesElements}</div>
+      <div className={s.textArea}>
+        <textarea ref={newMessage}></textarea>
+        <div>
+          <button onClick={addNewMessage} className={s.button}>
+            Add new message
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
