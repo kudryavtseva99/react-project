99b3c0ad453ad349ffe4f34e88adf521f5a8bd4e
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-06-20 18:19:50 +0200

    create obj store: - move state and funcs(methods) in store: - use bind

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  addPost,
-  updateNewPostText,
-  addNewMessage,
-  updateNewMessageText,
-} from "./redux/state.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -19,17 +13,17 @@ let rerenderEntireTree = (state) => {
     <React.StrictMode>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
-        addNewMessage={addNewMessage}
-        updateNewMessageText={updateNewMessageText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addNewMessage={store.addNewMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
       />
     </React.StrictMode>
   );
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,134 +1,131 @@
-let rerenderEntireTree = () => {
-  console.log("state changed");
-};
-
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: "It's my first post", likesCount: 10 },
-      {
-        id: 2,
-        message: "I want to tell you something interesting",
-        likesCount: 15,
-      },
-    ],
-    newPostText: "New something",
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: "It's my first post", likesCount: 10 },
+        {
+          id: 2,
+          message: "I want to tell you something interesting",
+          likesCount: 15,
+        },
+      ],
+      newPostText: "New something",
+    },
+    dialogsPage: {
+      messagesData: [
+        { id: 1, message: "Hi! How are you?" },
+        { id: 2, message: "I want to tell you about my day." },
+        { id: 3, message: "What's your fav hobby?" },
+        { id: 4, message: "See u soon" },
+        { id: 5, message: "I'm late. Sorry" },
+        { id: 6, message: "Tomorrow" },
+        { id: 7, message: "Think about it" },
+      ],
+      dialogsData: [
+        {
+          id: 1,
+          name: "Dima",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+        {
+          id: 2,
+          name: "Sasha",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+        {
+          id: 3,
+          name: "Artur",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+        {
+          id: 4,
+          name: "Julia",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+        {
+          id: 5,
+          name: "Roma",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+        {
+          id: 6,
+          name: "Elena",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+        {
+          id: 7,
+          name: "Nastya",
+          avatar:
+            "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
+        },
+      ],
+      newMessageText: "It's great day today!",
+    },
+    sidebar: {
+      friends: [
+        {
+          id: 1,
+          name: "Artur",
+          avatar:
+            "https://img.freepik.com/premium-vector/minimalist-ink-drawing-samurai-warrior-standing-profile_518698-2435.jpg",
+        },
+        {
+          id: 2,
+          name: "Julia",
+          avatar:
+            "https://img.freepik.com/premium-vector/geisha-silhouette-cyberpunk-pattern-black-white-illustration_894890-28643.jpg",
+        },
+        {
+          id: 3,
+          name: "Nastya",
+          avatar:
+            "https://img.freepik.com/premium-vector/woman-kimono-sits-front-tree-with-flowers_959938-128.jpg",
+        },
+      ],
+    },
   },
-  dialogsPage: {
-    messagesData: [
-      { id: 1, message: "Hi! How are you?" },
-      { id: 2, message: "I want to tell you about my day." },
-      { id: 3, message: "What's your fav hobby?" },
-      { id: 4, message: "See u soon" },
-      { id: 5, message: "I'm late. Sorry" },
-      { id: 6, message: "Tomorrow" },
-      { id: 7, message: "Think about it" },
-    ],
-    dialogsData: [
-      {
-        id: 1,
-        name: "Dima",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-      {
-        id: 2,
-        name: "Sasha",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-      {
-        id: 3,
-        name: "Artur",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-      {
-        id: 4,
-        name: "Julia",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-      {
-        id: 5,
-        name: "Roma",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-      {
-        id: 6,
-        name: "Elena",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-      {
-        id: 7,
-        name: "Nastya",
-        avatar:
-          "https://img.freepik.com/premium-vector/kitsune-mask-cartoon-character-icon-design_409025-876.jpg",
-      },
-    ],
-    newMessageText: "It's great day today!",
+  getState() {
+    return this._state;
   },
-  sidebar: {
-    friends: [
-      {
-        id: 1,
-        name: "Artur",
-        avatar:
-          "https://img.freepik.com/premium-vector/minimalist-ink-drawing-samurai-warrior-standing-profile_518698-2435.jpg",
-      },
-      {
-        id: 2,
-        name: "Julia",
-        avatar:
-          "https://img.freepik.com/premium-vector/geisha-silhouette-cyberpunk-pattern-black-white-illustration_894890-28643.jpg",
-      },
-      {
-        id: 3,
-        name: "Nastya",
-        avatar:
-          "https://img.freepik.com/premium-vector/woman-kimono-sits-front-tree-with-flowers_959938-128.jpg",
-      },
-    ],
+  _callSubscriber() {
+    console.log("state changed");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  addNewMessage() {
+    let newMessage = {
+      id: 8,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newMes) {
+    this._state.dialogsPage.newMessageText = newMes;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-// посты
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-// диалоги
-export const addNewMessage = () => {
-  let newMessage = {
-    id: 8,
-    message: state.dialogsPage.newMessageText,
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  state.dialogsPage.newMessageText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newMes) => {
-  state.dialogsPage.newMessageText = newMes;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
