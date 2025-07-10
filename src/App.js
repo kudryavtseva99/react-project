9b1975f67d97c5311cbe9bd51a6df9726efbfde7
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-07-10 15:44:58 +0200

    add authorization logic: -  create header Container component - add authReducer for storing auth state - implement axios request with credentials to fetch user auth data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -8,16 +7,17 @@ import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 function App(props) {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
           <Route path="/dialogs/*" element={<DialogsContainer />} />
-          <Route path="/profile/:userId" element={<ProfileContainer />} />
+          <Route path="/profile/:userId?" element={<ProfileContainer />} />
           <Route path="/news" element={<News />} />
           <Route path="/music" element={<Music />} />
           <Route path="/settings" element={<Settings />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
-import s from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <header className={s.header}>
+    <header className={styles.header}>
       <img
         src="https://img.freepik.com/premium-vector/hanami-japanese-flower-logo-design-cherry-blossom-vector-illustration_691652-143.jpg"
         alt=""
       ></img>
+      <div className={styles.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
