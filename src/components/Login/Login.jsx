cb172cb4e1aafabd377861d618e77a38e6e9b48b
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2025-10-28 21:54:57 +0100

    feat(login): create login form using redux-form with Field components and handleSubmit
    -add LoginForm and LoginReduxForm components
    - integrate redux-form to manage form state
    -implement handleSubmit for form data handling
    - display login, password, and rememberMe fields

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,39 @@
+import { Field, reduxForm } from "redux-form";
 import styles from "./Login.module.css";
 
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field name={"login"} placeholder="Login" component={"input"} />
+      </div>
+      <div>
+        {" "}
+        <Field name={"password"} placeholder="Password" component={"input"} />
+      </div>
+      <div>
+        {" "}
+        <Field name={"rememberMe"} component={"input"} type={"checkbox"} />{" "}
+        remember me
+      </div>
+      <div>
+        <button>Log in</button>
+      </div>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
+
 const Login = (props) => {
+  const onSubmit = (formData) => {
+    console.log("Form Data:", formData);
+  };
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginBox}>
         <h1 className={styles.loginTitle}>Login</h1>
-        {/* Здесь позже добавятся input и кнопки */}
+        <LoginReduxForm onSubmit={onSubmit} />
       </div>
     </div>
   );
