6731d38e969d2960884c6be9039d821f09126487
kudryavtsevaa99 <kudryyaaa@yandex.ru>
2026-02-05 18:43:25 +0100

    Add gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
